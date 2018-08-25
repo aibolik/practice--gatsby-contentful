e6075f6beb68dbe816bd81b73e471a27dffa5536
Aibol Kussain <aibolikdev@gmail.com>
2018-08-25 14:39:47 +0300

    feat: add integration with Contentful to get blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'w1fxlmsi3y1l',
+        accessToken: 'e1d39d4021699cce4d717eb50dce2f894def2d37ba999b3ec1b1efe04d3eb778'
+      }
+    },
+    'gatsby-plugin-react-helmet',
+  ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,41 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require('path')
 
  // You can delete this file if you're not using it
+ exports.createPages = ({ graphql, boundActionCreators }) =>{
+  const { createPage } = boundActionCreators
+  return new Promise((resolve, reject) => {
+    const blogPostTemplate = path.resolve('src/templates/blog-post.js')
+    resolve(
+      graphql(`
+        {
+          allContentfulBlog (limit: 1000) {
+            edges {
+              node {
+                id
+                slug
+              }
+            }
+          }
+        }
+      `).then(result => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+        result.data.allContentfulBlog.edges.forEach(edge => {
+          createPage({
+            path: edge.node.slug,
+            component: blogPostTemplate,
+            context: {
+              slug: edge.node.slug,
+
+            }
+          })
+          return
+        })
+      })
+    )
+  })
+ }
